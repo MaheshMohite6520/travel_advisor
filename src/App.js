1df8fe0d963b4d36ac93881f76f691d845295c3a
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-03-02 12:42:02 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
-import { getPlacesData } from './api';
+import { getPlacesData, getWeatherData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
 
-    const [ places, setPlaces ] = useState([]);
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
 
     const [coordinates, setCoordinates] = useState({});
-    const [bounds, setBounds] = useState(null)
+    const [bounds, setBounds] = useState({})
+
+   
+    const [ weatherData, setWeatherData ] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+    const [ places, setPlaces ] = useState([]);
+
+    const [childClicked, setChildClicked] = useState(null);
+    const [isLoading, setIsLoading] = useState(false)
+    
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
@@ -20,25 +30,52 @@ const App = () => {
     }, [])
 
     useEffect(() => {
-        getPlacesData(bounds.sw, bounds.ne)
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+
+        setFilteredPlaces(filteredPlaces)
+    }, [rating])
+
+    useEffect(() => {
+        if(bounds.sw && bounds.ne) {
+
+        setIsLoading(true)
+
+        getWeatherData(coordinates)
+            .then((data) => setWeatherData(data))
+
+        getPlacesData(type, bounds.sw, bounds.ne)
         .then((data) => {
-            setPlaces(data)
+            setPlaces(data?.filter((place) => place.name && place.num_reviews > 0))
+            setFilteredPlaces([])
+            setIsLoading(false)
         })
-    }, [coordinates, bounds])
+    }
+    }, [type, bounds])
 
     return (
         <>
             <CssBaseline />
-            <Header />
+            <Header setCoordinates={setCoordinates} />
             <Grid container spacing={3} stylr={{width: '100%'}}>
                 <Grid item xs={12} md={4}>
-                    <List places={places} />
+                    <List 
+                        places={filteredPlaces.length ? filteredPlaces : places} 
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                    />
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map 
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked={setChildClicked}
+                        WeatherData = {weatherData}
                     />
                 </Grid>
             </Grid>
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,8 @@
 import axios from "axios"; 
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
-
-export const getPlacesData = async (sw, ne) =>  {
+export const getPlacesData = async (type, sw, ne) =>  {
     try {
-        const { data : { data } } = await axios.get(URL, {
+        const { data : { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
             params: {
               bl_latitude: sw.lat,
               tr_latitude: ne.lat,
@@ -23,3 +21,21 @@ export const getPlacesData = async (sw, ne) =>  {
     }
 
 }
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://open-weather13.p.rapidapi.com/city/latlon/30.438/-89.1028', {
+        params: { lat: lat, lon: lng },
+        headers: {
+            'X-RapidAPI-Key': 'b98e60e025msha4d33f7c031168ap149692jsn0ebde160ae30',
+            'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+        },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
